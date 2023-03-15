0af0ed2027a895c018407a7ccb6ce817b769eb77
JubelHossain <jubel_hossain@appnap.io>
2023-03-15 11:55:49 +0600

    initial set up

---
+++ CalendarUI/CalendarViewController.swift
@@ -0,0 +1,197 @@
+//
+//  ViewController.swift
+//  CalendarUI
+//
+//  Created by Appnap WS13 on 3/14/23.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController,SKUIDatePickerDelegate {
+    private var isEndTextFieldSelected : Bool = false
+    lazy var chooseRangeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Choose range of dates", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(self.chooseRange), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var startTextField : UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Enter Start Date"
+        textField.keyboardType = UIKeyboardType.default
+        textField.returnKeyType = UIReturnKeyType.done
+        textField.autocorrectionType = UITextAutocorrectionType.no
+        textField.font = UIFont.systemFont(ofSize: 13)
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        textField.clearButtonMode = UITextField.ViewMode.whileEditing;
+        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        return textField
+    }()
+    
+    lazy var endDateTxtField : UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Enter End Date"
+        textField.keyboardType = UIKeyboardType.default
+        textField.returnKeyType = UIReturnKeyType.done
+        textField.autocorrectionType = UITextAutocorrectionType.no
+        textField.font = UIFont.systemFont(ofSize: 13)
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        textField.clearButtonMode = UITextField.ViewMode.whileEditing;
+        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        textField.addTarget(self, action: #selector(myTargetFunction), for: .touchDown)
+        return textField
+    }()
+    
+    private var skUIdatePicker: SKUIDatePicker?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        self.configureUI()
+        self.configureSubviews()
+        
+        skUIdatePicker = SKUIDatePicker()
+        skUIdatePicker!.delegate = self
+        skUIdatePicker!.showDatePicker(txtDatePicker: startTextField)
+        //skUIdatePicker!.showDatePicker(txtDatePicker: endDateTxtField)
+    }
+    
+    @objc func myTargetFunction(textField: UITextField) {
+        print("myTargetFunction")
+        isEndTextFieldSelected = true
+        skUIdatePicker!.showDatePicker(txtDatePicker: endDateTxtField)
+    }
+    private func configureUI() {
+        self.view.backgroundColor = .systemBackground
+        self.navigationItem.title = "Celendar demo"
+        self.navigationItem.largeTitleDisplayMode = .always
+    }
+    
+    private func configureSubviews() {
+        self.view.addSubview(self.chooseRangeButton)
+        self.view.addSubview(startTextField)
+        self.view.addSubview(endDateTxtField)
+        chooseRangeButton.centerX(inView: self.view)
+        chooseRangeButton.centerY(inView: self.view)
+        startTextField.anchorView(top: chooseRangeButton.bottomAnchor,paddingTop: 30)
+        startTextField.centerX(inView: self.view)
+        startTextField.setDimensions(width: 200, height: 50)
+        
+        endDateTxtField.anchorView(top: startTextField.bottomAnchor,paddingTop: 30)
+        endDateTxtField.centerX(inView: self.view)
+        endDateTxtField.setDimensions(width: 200, height: 50)
+    }
+    
+    func datesRange(from: Date, to: Date) -> [Date] {
+        // in case of the "from" date is more than "to" date,
+        // it should returns an empty array:
+        if from > to { return [Date]() }
+        
+        var tempDate = from
+        var array = [tempDate]
+        
+        while tempDate < to {
+            tempDate = Calendar.current.date(byAdding: .day, value: 1, to: tempDate)!
+            array.append(tempDate)
+        }
+        
+        return array
+    }
+    
+    @objc
+    private func chooseRange() {
+        let today = Date()
+        let nextFiveDays = Calendar.current.date(byAdding: .day, value: 5, to: today)!
+        
+        let myRange = datesRange(from: today, to: nextFiveDays)
+        print(myRange)
+        //        let fastisController = FastisController(mode: .range)
+        //        fastisController.title = "Choose range"
+        //        fastisController.initialValue = self.currentValue as? FastisRange
+        //        fastisController.minimumDate = Calendar.current.date(byAdding: .month, value: -1, to: Date())
+        //        print("minimum Data : \(fastisController.minimumDate)")
+        //        fastisController.maximumDate = Calendar.current.date(byAdding: .month, value: 1, to: Date())
+        //        print("maximumDate Data : \(fastisController.maximumDate)")
+        //        fastisController.allowToChooseNilDate = true
+        //       // fastisController.shortcuts = [.today, .lastWeek, .lastMonth]
+        //        fastisController.doneHandler = { newValue in
+        //            self.currentValue = newValue
+        //        }
+        //        fastisController.present(above: self)
+    }
+    
+    func getDate(_ sKUIDatePicker:SKUIDatePicker, date:String) {
+        print(date)
+        if isEndTextFieldSelected {
+            endDateTxtField.text = "\(date)"
+        }
+        else {
+            startTextField.text = "\(date)"
+        }
+        
+        self.view.endEditing(true)
+    }
+    func cancel(_ sKUIDatePicker:SKUIDatePicker){
+        self.view.endEditing(true)
+    }
+}
+
+
+import UIKit
+
+protocol SKUIDatePickerDelegate:class {
+    func getDate(_ sKUIDatePicker:SKUIDatePicker, date:String)
+    func cancel(_ sKUIDatePicker:SKUIDatePicker)
+}
+
+class SKUIDatePicker:UIView {
+    
+    private let datePicker = UIDatePicker()
+    private var dateFormate = "dd/MM/yyyy"
+    weak var delegate:SKUIDatePickerDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // self.frame = UIScreen.main.bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func showDatePicker(txtDatePicker:UITextField){
+        //Formate Date
+        datePicker.datePickerMode = .date
+        
+        //ToolBar
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action:       #selector(donedatePicker));
+        let spaceButton = UIBarButtonItem(barButtonSystemItem:       UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action:       #selector(cancelDatePicker));
+        
+        toolbar.setItems([doneButton,spaceButton,cancelButton], animated:       false)
+        
+        txtDatePicker.inputAccessoryView = toolbar
+        txtDatePicker.inputView = datePicker
+        
+    }
+    
+    @objc func donedatePicker(){
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = dateFormate
+        let result = formatter.string(from: datePicker.date)
+        self.delegate?.getDate(self, date: result)
+        
+    }
+    
+    @objc func cancelDatePicker(){
+        self.delegate?.cancel(self)
+    }
+    
+}
---
+++ CalendarUI/Support/UIView+Ext.swift
@@ -0,0 +1,151 @@
+//
+//  UIView+Ext.swift
+//  CalendarUI
+//
+//  Created by Appnap WS13 on 3/15/23.
+//
+
+import UIKit
+extension UIView {
+    func anchor (top : NSLayoutYAxisAnchor? , left: NSLayoutXAxisAnchor? , bottom : NSLayoutYAxisAnchor? , right : NSLayoutXAxisAnchor? , paddingTop : CGFloat , paddingLeft : CGFloat , paddingBottom : CGFloat , paddingRight : CGFloat , width : CGFloat , height : CGFloat){
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top , constant: paddingTop).isActive = true
+        }
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left , constant: paddingLeft).isActive = true
+        }
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom , constant: -paddingBottom).isActive = true
+        }
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right , constant: -paddingRight).isActive = true
+        }
+        if  width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        
+    }
+    
+    func fillSuperview(padding: UIEdgeInsets = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewTopAnchor = superview?.topAnchor {
+            topAnchor.constraint(equalTo: superviewTopAnchor, constant: padding.top).isActive = true
+        }
+        
+        if let superviewBottomAnchor = superview?.bottomAnchor {
+            bottomAnchor.constraint(equalTo: superviewBottomAnchor, constant: -padding.bottom).isActive = true
+        }
+        
+        if let superviewLeadingAnchor = superview?.leadingAnchor {
+            leadingAnchor.constraint(equalTo: superviewLeadingAnchor, constant: padding.left).isActive = true
+        }
+        
+        if let superviewTrailingAnchor = superview?.trailingAnchor {
+            trailingAnchor.constraint(equalTo: superviewTrailingAnchor, constant: -padding.right).isActive = true
+        }
+    }
+    
+    func centerInSuperview(size: CGSize = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+        
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+    
+    func anchorView(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func centerX(inView view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func centerY(inView view: UIView, leftAnchor: NSLayoutXAxisAnchor? = nil,
+                 paddingLeft: CGFloat = 0, constant: CGFloat = 0) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant).isActive = true
+        
+        if let lefts = leftAnchor {
+            anchorView(left: lefts, paddingLeft: paddingLeft)
+        }
+    }
+    
+    public var width: CGFloat{
+        return frame.size.width
+    }
+    public var height: CGFloat{
+           return frame.size.height
+       }
+    public var top: CGFloat{
+        return frame.origin.y
+       }
+    public var bottom: CGFloat{
+        return frame.origin.y + frame.size.height
+       }
+    public var left: CGFloat{
+        return frame.origin.x
+       }
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+    
+    func setDimensions(width: CGFloat, height: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+}
